d5b029ce49bc85c9e3e612b2b509d81e88369989
dsladkov <dimasladkov@gmail.com>
2018-07-07 16:32:17 +0300

    Добавьте файлы проекта.

---
+++ Prediction/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CSPredictionSample
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter image file path: ");
+
+            string imageFilePath = @"C:\Users\happyPanda\Desktop\AZURE\InsulatedTestJacked\112543.jpeg";//Console.ReadLine();
+
+            MakePredictionRequest(imageFilePath).Wait();
+
+            Console.WriteLine("\n\n\nHit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        static async Task MakePredictionRequest(string imageFilePath)
+        {
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid subscription key.
+            client.DefaultRequestHeaders.Add("Prediction-Key", "596f5944c7ec4057975c1176a7921489");
+
+            // Prediction URL - replace this example URL with your valid prediction URL.
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/32755ed0-1f6f-4bf9-9d0c-224fc1e77101/image?iterationId=c7c5bb0d-9053-458e-b4d3-b1d1b5e99b72";
+
+            HttpResponseMessage response;
+
+            // Request body. Try this sample with a locally stored image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
